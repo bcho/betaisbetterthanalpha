88a8ea7ec15a01dedbb229b0c021ba8627c247e8
hbc <bcxxxxxx@gmail.com>
2013-08-06 21:41:53 +0800

    added simple TCP handler wrapper

--- handler.py
+++ handler.py
@@ -1,16 +1,63 @@
 #coding: utf-8
 
+import sqlite3
 import SocketServer
 from ConfigParser import ConfigParser
 
 
 config = ConfigParser()
 config.read('config.ini')
-BUFSIZE = config.get('relay', 'bufsize')
+BUFSIZE = int(config.get('relay', 'bufsize'))
 
 
 class RequestHandler(SocketServer.BaseRequestHandler):
+    '''A simple TCP request handler for communicating
+    with ardunio client.'''
+
+    def setup(self):
+        self.connection = sqlite3.connect(config.get('db', 'name'))
+
+    def finish(self):
+        self.connection.commit()
+        self.connection.close()
 
     def handle(self):
-        data = self.request.recv(BUFSIZE)
-        self.request.sendall(data)
+        '''Just forward all the operations to wechat layer'''
+
+        payload = self.request.recv(BUFSIZE)
+        self.store_stats(payload.strip())
+
+        command = self.get_command()
+        if command:
+            self.request.sendall(command['value'])
+            self.consume_command(command['id'])
+
+    def consume_command(self, _id):
+        '''mark a command as consumed'''
+
+        cursor = self.connection.cursor()
+        cursor.execute('''UPDATE `jobs`
+        SET `consumed` = 1
+        WHERE `id` = :id;''', {'id': _id})
+
+    def get_command(self):
+        '''get one latest command'''
+
+        def _decorate(row):
+            if not row:
+                return {}
+            return {
+                'id': row[0],
+                'value': row[1]
+            }
+
+        cursor = self.connection.cursor()
+        cursor.execute('''SELECT * FROM `jobs`
+            WHERE `consumed` = 0
+            ORDER BY `created` DESC LIMIT 1;''')
+        return _decorate(cursor.fetchone())
+
+    def store_stats(self, stats):
+        cursor = self.connection.cursor()
+        cursor.execute('''INSERT INTO `stats` (`value`)
+        VALUES(:value)''', {'value': stats})
--- schema.sql
+++ schema.sql
@@ -1,8 +1,19 @@
 DROP TABLE IF EXISTS `jobs`;
+DROP TABLE IF EXISTS `stats`;
 
 /* jobs queue */
 CREATE TABLE `jobs` (
     `id` integer PRIMARY KEY,
+    /* command */
+    `value` varchar(1024) NOT NULL,
+    `consumed` TINYINT(1) DEFAULT 0,
+    `created` timestamp DEFAULT CURRENT_TIMESTAMP
+);
+
+/* sensor stats collection */
+CREATE TABLE `stats` (
+    `id` integer PRIMARY KEY,
+    /* sensor stats */
     `value` varchar(1024) NOT NULL,
     `consumed` TINYINT(1) DEFAULT 0,
     `created` timestamp DEFAULT CURRENT_TIMESTAMP
