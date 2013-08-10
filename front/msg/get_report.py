8580c6528138806f6688ef8199b6b346b94c27e7
hbc <bcxxxxxx@gmail.com>
2013-08-10 22:58:50 +0800

    enhanced report

--- front/msg/get_report.py
+++ front/msg/get_report.py
@@ -5,10 +5,10 @@ __all__ = ['get_report', 'report_not_found']
 
 def get_report(value):
     return u'''现在的环境温度：%s ℃
-      湿度: %s%%
-      固体颗粒物浓度： %s μg/㎥
-      甲醛气体浓度： %s mg/㎥ ''' % (value['t'], value['h'],
-                                     value['pm'], value['ch'])
+湿度: %s%%
+固体颗粒物浓度： %s μg/㎥
+甲醛气体浓度： %s mg/㎥ ''' % (value['t'], value['h'],
+                               value['pm'], value['ch'])
 
 
 def report_not_found():
