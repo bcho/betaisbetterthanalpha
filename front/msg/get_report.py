76684529ed834853ae5ba39b32e95c8af7b945d9
hbc <bcxxxxxx@gmail.com>
2013-08-10 22:41:07 +0800

    fixed typo

--- front/msg/get_report.py
+++ front/msg/get_report.py
@@ -4,9 +4,9 @@ __all__ = ['get_report', 'report_not_found']
 
 
 def get_report(value):
-    return u'现在的环境温度：%r  湿度: %s  '
-    '固体颗粒物浓度： %s  甲醛气体浓度： %s' % (value['t'], value['h'],
-                                                value['pm'], value['ch'])
+    return u'''现在的环境温度：%s  湿度: %s
+固体颗粒物浓度： %s  甲醛气体浓度： %s''' % (value['t'], value['h'],
+                                             value['pm'], value['ch'])
 
 
 def report_not_found():
