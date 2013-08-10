ed01113a272bb8b732721a422a64a21989c61617
hbc <bcxxxxxx@gmail.com>
2013-08-10 20:03:06 +0800

    added fake report

--- front/action/get_report.py
+++ front/action/get_report.py
@@ -2,6 +2,29 @@
 
 __all__ = ['get_report']
 
+import sqlite3
+
+from front import config
+
 
 def get_report():
-    return u'湿度：95% 温度：37 摄氏度'
+
+    def _decorate(row):
+        if not row:
+            return {}
+        return {
+            'id': row[0],
+            'value': row[1]
+        }
+
+    connection = sqlite3.connect(config.DB_PATH)
+    cursor = connection.cursor()
+
+    cursor.execute('''SELECT * FROM `stats`
+        WHERE `consumed` = 0
+        LIMIT 1
+        ORDER BY `created` DESC LIMIT 1;''')
+
+    ret = _decorate(cursor.fetchone())
+
+    return u'值: %s' % ret['value']
