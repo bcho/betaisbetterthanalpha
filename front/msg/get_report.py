9f54451245ef44a00f1354d2f81b784a4f425efc
hbc <bcxxxxxx@gmail.com>
2013-08-11 10:41:29 +0800

    added some fake data

--- front/msg/get_report.py
+++ front/msg/get_report.py
@@ -2,12 +2,15 @@
 
 __all__ = ['get_report', 'report_not_found']
 
+import random
+
 
 def get_report(value):
     return u'''现在的湿度:%s%%
 环境温度:%s ℃
 固体颗粒物浓度:%s μg/㎥
-甲醛气体浓度:%s mg/㎥ ''' % (value['h'], value['t'], value['pm'], value['ch'])
+甲醛气体浓度:%s mg/㎥ ''' % (value['h'], value['t'], random.randint(25, 35),
+                             value['ch'])
 
 
 def report_not_found():
